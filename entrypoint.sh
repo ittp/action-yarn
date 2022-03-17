91792433c44603f9dd7b4f699559b5fe14563206
ittp <61617660+ittp@users.noreply.github.com>
2022-03-17 14:15:42 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,13 @@ set -e
 
 echo "Starting..."
 
-npm install -g yarn
+npm install -g yarn pm2
 
 sh -c "$*"
+
+YARN_VER=$(yarn -v)
+PM2_VER=$(pm2 -v)
+echo $YARN_VER
+echo $PM2_VER
 
 echo "Finished!"
